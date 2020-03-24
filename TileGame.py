b7c9b6996bcd6745d4c247efe77c94827af00964
primayoriko <primayoriko@gmail.com>
2020-03-24 23:08:28 +0700

    add driver and fix tilegameClass

--- TileGame.py
+++ TileGame.py
@@ -1,12 +1,14 @@
 import math
 
 class TileGame:
-    def __init__(self, size=4, arr=[i for i in range(1,16)]):
+    def __init__(self, size=4, arr=[[0]*4 for _ in range(4)]):
         self.size = size
-        self.elmt =  [0 for i in range(size)] * size
+        self.elmt = [[0]*size for _ in range(size)]
         for i in range(size):
             for j in range(size):
-                self.elmt[i][j] = arr[i*size + j]
+                if(arr[i][j]==0):
+                    self.blank_x, self.blank_y = j, i
+                self.elmt[i][j] = arr[i][j]
 
     def copyTile(self, other):
         self.size = other.size
@@ -14,42 +16,45 @@ class TileGame:
             for j in range(self.size):
                 self.elmt[i][j] = other.elmt[i][j]
 
-
     def moveBlankDown(i, j=-1):
         if(j==-1):
-            y = i//self.size
-            x = i%self.size
-            if(self.elmt[y][x]==0 and y != self.size-1):
-                self.elmt[y][x], self.elmt[y+1][x] = self.elmt[y+1][x], self.elmt[y][x]
-            else:
-                print("operasi Down di ", i, " tidak valid")
+            x,y =i%self.size, i//self.size
+        else:
+            x, y = j, i
+        if(self.elmt[y][x]==0 and y != self.size-1):
+            self.elmt[y][x], self.elmt[y+1][x] = self.elmt[y+1][x], self.elmt[y][x]
+        else:
+            print("operasi Down di ", i, " tidak valid")
 
     def moveBlankUp(i, j=-1):
         if(j==-1):
-            y = i//self.size
-            x = i%self.size
-            if(self.elmt[y][x]==0 and y != 0):
-                self.elmt[y][x], self.elmt[y-1][x] = self.elmt[y-1][x], self.elmt[y][x]
-            else:
-                print("operasi Up di ", i, " tidak valid")
+            x,y =i%self.size, i//self.size
+        else:
+            x, y = j, i
+        if(self.elmt[y][x]==0 and y != 0):
+            self.elmt[y][x], self.elmt[y-1][x] = self.elmt[y-1][x], self.elmt[y][x]
+        else:
+            print("operasi Up di ", i, " tidak valid")
 
     def moveBlankLeft(i, j=-1):
         if(j==-1):
-            y = i//self.size
-            x = i%self.size
-            if(self.elmt[y][x]==0 and x != 0):
-                self.elmt[y][x], self.elmt[y][x-1] = self.elmt[y][x-1], self.elmt[y][x]
-            else:
-                print("operasi Left di ", i, " tidak valid")
+            x,y =i%self.size, i//self.size
+        else:
+            x, y = j, i
+        if(self.elmt[y][x]==0 and x != 0):
+            self.elmt[y][x], self.elmt[y][x-1] = self.elmt[y][x-1], self.elmt[y][x]
+        else:
+            print("operasi Left di ", i, " tidak valid")
 
     def moveBlankRight(i, j=-1):
         if(j==-1):
-            y = i//self.size
-            x = i%self.size
-            if(self.elmt[y][x]==0 and x != self.size):
-                self.elmt[y][x], self.elmt[y][x+1] = self.elmt[y][x+1], self.elmt[y][x]
-            else:
-                print("operasi Right di ", i, " tidak valid")
+            x,y =i%self.size, i//self.size
+        else:
+            x, y = j, i
+        if(self.elmt[y][x]==0 and x != self.size):
+            self.elmt[y][x], self.elmt[y][x+1] = self.elmt[y][x+1], self.elmt[y][x]
+        else:
+            print("operasi Right di ", i, " tidak valid")
 
     def countUnmatch(self, compares):
         cnt = 0
@@ -59,12 +64,19 @@ class TileGame:
                        else cnt
         return cnt
 
-    def fungsiKurang(self, compares, index): #I don't know the english of it
-        for i in range(self.size**2):
-            if(self.elmt[i//self.size][i%self.size]==index):
-                x = i%self.size
-                y = i//self.size
-                break
+    def fungsiKurang(self):
+        sums = 0
+        for i in range(4):
+            for j in range(4):
+                if(i == self.blank_y and j == self.blank_x):
+                    # print(i, j)
+                    sums += ((i+j)%2)
+                else:
+                    for k in range(i*self.size + j+1, 16):
+                        if(self.elmt[k//self.size][k%self.size]<self.elmt[i][j]):
+                            # print(i, j, k//self.size, k%self.size)
+                            sums+=1 if (k//self.size != self.blank_y and k%self.size != self.blank_x) else 0
+        return sums
 
     def printElmt(self):
         for i in range(self.size):
---
+++ driver.py
@@ -0,0 +1,30 @@
+from TileGame import *
+
+def inputTile(param, tile, filename="tile.txt"):
+    if(param==2):
+        file = open(filename, "r")
+    for i in range(4):
+        line = input() if param==1 else file.readline()
+        val = line.split()
+        val = [int(val[j]) for j in range(len(val))]
+        tile[i] = val
+    file.close()
+
+if __name__=='__main__':
+    # param = int(input('TileGame Solver\n\nInput:\n 1. Konsol\n 2. File\n\nMasukkan tipe input: '))
+    # filename = input('TileGame\n\nMasukkan nama file: ')
+    param = 2
+    arr=[[0]*4 for _ in range(4)]
+    arrGoal=[]
+    for i in range(4):
+        arrTemp=[]
+        for j in range(4):
+            arrTemp.append((i*4 +j+1)%16)
+        arrGoal.append(arrTemp)
+    inputTile(param=param, tile=arr)
+    tileGoal = TileGame(arr=arrGoal)
+    tile = TileGame(arr=arr)
+    tile.printElmt()
+    print(tile.blank_x, tile.blank_y)
+    tileGoal.printElmt()
+    print(tile.countUnmatch(tileGoal))
